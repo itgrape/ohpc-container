f51c5169a1bf911309a7540fd86cabd4b1a272c6
itgrape <itgrape@outlook.com>
2025-08-13 09:08:08 +0800

    update: optimize build.sh, add sshd_config, reorder steps

--- 02-node-network/build.sh
+++ 02-node-network/build.sh
@@ -29,10 +29,6 @@ buildah run "${ctr}" -- bash -c '
   dnf install -y openssh-server
   dnf swap -y curl-minimal curl
   dnf install -y git wget tmux screen rsync iproute iputils
-
-  echo ">>> Cleaning up package cache..."
-  dnf clean all
-  rm -rf /var/cache/dnf/*
 '
 
 # 4. LDAP 用户认证
@@ -44,28 +40,35 @@ buildah run "${ctr}" -- bash -c '
   echo ">>> Configure LDAP Client"
   dnf install -y openldap-clients nss-pam-ldapd
   chmod 640 /etc/nslcd.conf
+
+  echo ">>> Cleaning up package cache..."
+  dnf clean all
+  rm -rf /var/cache/dnf/*
 '
 
-# 5. 设置 root 密码
+# 5. openssh 配置
+buildah copy "${ctr}" ./ssh_config/sshd_config /etc/ssh/sshd_config
+
+# 6. 设置 root 密码
 buildah run "${ctr}" -- bash -c 'usermod -p "$(openssl passwd -1 -stdin <<< root)" root'
 
-# 6. 配置开机任务
+# 7. 配置开机任务
 buildah run "${ctr}" -- bash -c '
   echo "rm -f /var/run/nologin" >> /etc/rc.local
   chmod +x /etc/rc.local
 '
 
-# 7. 启用 systemd 服务
+# 8. 启用 systemd 服务
 buildah run "${ctr}" -- systemctl enable sshd nslcd
 
-# 8. 设置容器默认启动命令
+# 9. 设置容器默认启动命令
 buildah config --cmd '["/usr/sbin/init"]' "${ctr}"
 
-# 9. 提交工作容器为新镜像
+# 10. 提交工作容器为新镜像
 echo "--- Committing ${NEW_IMAGE_NAME} ---"
 buildah commit "${ctr}" "${NEW_IMAGE_NAME}"
 
-# 10. 清理临时工作容器
+# 11. 清理临时工作容器
 buildah rm "${ctr}"
 
 echo "--- Build complete for ${NEW_IMAGE_NAME} ---"
